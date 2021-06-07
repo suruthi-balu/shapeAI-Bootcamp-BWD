544e815c3df5af1d2a4ab9aeb80153036943f916
suruthi-balu <85474407+suruthi-balu@users.noreply.github.com>
2021-06-07 20:40:15 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Shape-AI  BOOTCAMPS</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> BASIC WEB DEVELOPMENT</h1>
+      <p>
+      A 2.5 month long series of LIVE classes with recorded sessions, teaching assistants, and mentors.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
+  
